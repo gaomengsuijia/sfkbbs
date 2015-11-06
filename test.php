451169100faf7227e1c04e9b76a8c063d0e201a2
xiaotu <hulinjun2006@126.com>
2015-11-06 16:47:50 +0800

    commit by dev

--- test.php
+++ test.php
@@ -1,24 +1,11 @@
 <?php
-class goushi{
-	var $name;
-	var $age;
-	function __construct($name,$age){
-		$this->name=$name;
-		$this->age=$age;
-	}
-}
 class Person{
-	private $redis;
-	function __construct($name,$age){
-		$this->redis=new goushi();
-		return $this->redis;
+	var $age = 10;
+	function say(){
+		echo $this->age;
 	}
-	
-}
-//$b = new goushi('li','lu');
-$a = new Person('li','lu');
-
-var_dump($a);
-
+};
+$son = new Person();
+var_dump($son->age);
 
 ?>
